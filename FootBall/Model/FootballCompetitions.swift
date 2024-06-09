c6faa0f0d2aba44113ff4443fbeeb88698ca95fe
MinaEmad6060 <minaemad6060@gmail.com>
2024-06-09 16:36:54 -0700

    create FootballCompetitions model

---
+++ FootBall/Model/FootballCompetitions.swift
@@ -0,0 +1,43 @@
+//
+//  Leagues.swift
+//  FootBall
+//
+//  Created by Mina Emad on 09/06/2024.
+//
+
+import Foundation
+
+struct FootballCompetitions: Decodable{
+    var competitions: [Competition]?
+}
+
+struct Competition: Decodable{
+    var id: Int?
+    var area: Area?
+    //longName
+    var name: String?
+    var emblemUrl: String?
+    var currentSeason: Season?
+}
+
+
+struct Area: Decodable{
+    //shortName
+    var name: String?
+}
+
+struct Season: Decodable{
+    //games
+    var currentMatchday: Int?
+    var startDate: String?
+    var endDate: String?
+    var winner: Winner?
+}
+
+struct Winner: Decodable{
+    //longName
+    var name: String?
+    //shortName
+    var tla: String?
+    var crestUrl: String?
+}
