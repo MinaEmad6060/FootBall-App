6f69c8e4ff117c6dc2405b626cf55ab312e97965
MinaEmad6060 <minaemad6060@gmail.com>
2024-06-09 12:40:04 -0700

    apply constrains for CompetitionViewCell xib file

--- FootBall/AllCompetitionsViewController.swift
+++ FootBall/AllCompetitionsViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class AllCompetitionsViewController: UIViewController {
+class AllCompetitionsViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
 
+    
+    @IBOutlet weak var competitionsTable: UITableView!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        competitionsTable.delegate = self
+        competitionsTable.dataSource = self
+        let nibCustomCell = UINib(nibName: "CompetitionViewCell", bundle: nil)
+        competitionsTable.register(nibCustomCell, forCellReuseIdentifier: "competitionCell")
     }
 
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "competitionCell", for: indexPath) as! CompetitionViewCell
+
+        return cell
+    }
+    
+    
 }
 
