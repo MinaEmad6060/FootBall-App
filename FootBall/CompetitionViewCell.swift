46c99809e7405ced8221f5a23fb619a393d0dc9d
MinaEmad6060 <minaemad6060@gmail.com>
2024-06-09 12:08:48 -0700

    create CocoaTouch file for CompetitionViewCell (Competition custom cell)

---
+++ FootBall/CompetitionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CompetitionViewCell.swift
+//  FootBall
+//
+//  Created by Mina Emad on 09/06/2024.
+//
+
+import UIKit
+
+class CompetitionViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
