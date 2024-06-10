0a99b117f2b55bf48876882ba3166260d01e170d
MinaEmad6060 <minaemad6060@gmail.com>
2024-06-10 06:24:42 -0700

    create PlayerViewCell

---
+++ FootBall/View/Classes/PlayerViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PlayerViewCell.swift
+//  FootBall
+//
+//  Created by Mina Emad on 10/06/2024.
+//
+
+import UIKit
+
+class PlayerViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
